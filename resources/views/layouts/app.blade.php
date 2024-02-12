535be98e700065473b7719f7822278b18a6032ee
Omar Elewa <omarelewa25@gmail.com>
2024-02-12 19:42:28 +0300

    added another logo shape

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo.png" />
+        <link rel="manifest" href="/logo.png" />
+        <meta name="description" content="Task Manager" />
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,11 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo.png" />
+        <link rel="manifest" href="/logo.png" />
+        <meta name="description" content="Task Manager" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
