67314ab6a25389f71907f4e18da29e2a721e649c
Omar Elewa <omarelewa25@gmail.com>
2024-02-13 01:07:53 +0300

    fix migration

--- database/migrations/2024_02_12_143732_create_projects_table.php
+++ database/migrations/2024_02_12_143732_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
