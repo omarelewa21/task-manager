2b06262f0716bceebf99641e9b5aeef1f679e1e9
Omar Elewa <omarelewa25@gmail.com>
2024-02-13 18:42:30 +0300

    fix web routes

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('tasks', TaskController::class)->except([
-    'show', 'edit'
-])->middleware(['auth', 'verified']);
-
-Route::get('dashboard', [TaskController::class, 'index'])
+Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
